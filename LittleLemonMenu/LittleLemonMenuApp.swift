bc37e2197050cbd2381ab58b730d83a28db80abe
tasaquino <thaisandrade@live.com>
2023-09-20 15:28:44 +0200

    Initial Commit

---
+++ LittleLemonMenu/LittleLemonMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonMenuApp.swift
+//  LittleLemonMenu
+//
+//  Created by Thais Aquino on 20/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
